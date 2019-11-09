a7f90505df535a82a60c5f06d2e61ee2649fb442
Eric Eaglstun <postpostmodern@gmail.com>
2019-11-09 01:54:00 -0700

    refine walk / jump

--- main.lua
+++ main.lua
@@ -5,15 +5,21 @@ local dragable = require "com.ponywolf.dragable"
 local vjoy = require "com.ponywolf.vjoy"
 
 physics.start()
-physics.setGravity(0, 20)
+physics.setGravity(0, 100)
+-- physics.setDrawMode( "hybrid" )
+
+local wall_dims = {
+    bounce = .1,
+    friction = 1
+}
 
 local floor = display.newRect(240, 320, display.actualContentWidth, 50)
 floor:setFillColor(0.2, 0.2, 1)
-physics.addBody(floor, "static", {bounce = .5})
+physics.addBody(floor, "static", wall_dims)
 
 local ceil = display.newRect(240, 0, display.actualContentWidth, 50)
 ceil:setFillColor(0.2, 0.2, 1)
-physics.addBody(ceil, "static", {bounce = .5})
+physics.addBody(ceil, "static", wall_dims)
 
 local wall_left =
     display.newRect(
@@ -23,47 +29,50 @@ local wall_left =
     display.actualContentHeight
 )
 wall_left:setFillColor(0.2, 0.2, 1)
-physics.addBody(wall_left, "static", {bounce = .5})
+physics.addBody(wall_left, "static", wall_dims)
 
 local wall_right =
     display.newRect(display.actualContentWidth - 50, display.actualContentHeight / 2, 50, display.actualContentHeight)
 wall_right:setFillColor(0.2, 0.2, 1)
-physics.addBody(wall_right, "static", {bounce = .5})
+physics.addBody(wall_right, "static", wall_dims)
 
 local bodies = {}
 
 local body_dims = {
-    start_radius = 4,
+    start_radius = 1,
     mid_radius = 12,
     units_count = 7
 }
 
 local body_half = math.ceil(body_dims.units_count / 2)
 
-print("start!")
 for i = 1, body_dims.units_count do
-    local increment = (body_dims.mid_radius - body_dims.start_radius) / body_dims.units_count
+    local increment = (body_dims.mid_radius - body_dims.start_radius) / (body_dims.units_count - 1)
     local arc = math.abs(i - body_half)
     local radius = body_dims.mid_radius - (arc * increment)
 
-    print("arc", arc)
-    print("radius", radius)
+    -- print("arc", arc)
+    -- print("radius", radius)
 
-    bodies[i] = display.newCircle(100, 200, radius)
-    bodies[i]:setFillColor(1, 0, 0, .5)
+    bodies[i] = display.newCircle(100 + (increment * i * 2), 100, radius)
+
+    bodies[i].isFixedRotation = true
+    bodies[i]:setFillColor(1, 1 / (body_dims.units_count - i + 1), 0, 1)
 
     physics.addBody(
         bodies[i],
         "dynamic",
         {
-            bounce = .75,
-            density = radius
+            bounce = 0,
+            density = 1,
+            friction = 1,
+            radius = radius
         }
     )
 
     if (i ~= 1) then
         local j = physics.newJoint("rope", bodies[i - 1], bodies[i], 0, 0, 0, 0)
-        j.maxLength = radius * 2
+        j.maxLength = radius 
     end
 
     if (i == 3) then
@@ -77,10 +86,11 @@ local axis = {
 }
 
 local function onAxis(event)
+    print(event.axis.number, event.normalizedValue)
     axis[event.axis.number] = event.normalizedValue
 
     local angle = math.deg(math.atan2(axis[1], axis[2]))
-    print("onAxis", event.axis.number, event.normalizedValue, math.floor(angle))
+    -- print("onAxis", event.axis.number, event.normalizedValue, math.floor(angle))
 end
 Runtime:addEventListener("axis", onAxis)
 
@@ -90,22 +100,52 @@ joy.x, joy.y = 10, display.actualContentHeight - 40
 local btn_space = vjoy.newButton("space", 20)
 btn_space.x, btn_space.y = display.actualContentWidth - 80, display.actualContentHeight - 40
 
-local function keyDown(event)
-    print("player keyDown event.keyName", event.keyName)
+local can_jump = {
+    0,
+    0,
+    0
+}
 
+local function keyDown(event)
+    print(event.keyName)
     if (event.phase == "down") then
         -- right
-        if (event.keyName == "space" or event.keyName == "button1") then
-            bodies[1]:applyForce(axis[1] * 10000, axis[2] * 10000, bodies[1].x, bodies[1].y)
-            bodies[body_half]:applyForce(axis[1] * 30000, axis[2] * 30000, bodies[body_half].x, bodies[body_half].y)
-            bodies[body_dims.units_count]:applyForce(
-                axis[1] * 10000,
-                axis[2] * 10000,
-                bodies[body_dims.units_count].x,
-                bodies[body_dims.units_count].y
-            )
+        if (event.keyName == "space" or event.keyName == "button1" or event.keyName == "buttonB") then
+            if (can_jump[1]) then
+                bodies[1]:applyForce(axis[1] * 500, axis[2] * 2500, bodies[1].x, bodies[1].y)
+            end
+
+            if (can_jump[2]) then
+                bodies[body_half]:applyForce(axis[1] * 1000, axis[2] * 2500, bodies[body_half].x, bodies[body_half].y)
+            end
+
+            if (can_jump[3]) then
+                bodies[body_dims.units_count]:applyForce(
+                    axis[1] * 500,
+                    axis[2] * 2500,
+                    bodies[body_dims.units_count].x,
+                    bodies[body_dims.units_count].y
+                )
+            end
         end
     end
 end
 
 Runtime:addEventListener("key", keyDown)
+
+local function onLocalCollision(self, event)
+    if (event.other == bodies[1]) then
+        can_jump[1] = event.phase == "began"
+    end
+
+    if (event.other == bodies[body_half]) then
+        can_jump[2] = event.phase == "began"
+    end
+
+    if (event.other == bodies[body_dims.units_count]) then
+        can_jump[3] = event.phase == "began"
+    end
+end
+
+floor.collision = onLocalCollision
+floor:addEventListener("collision")
